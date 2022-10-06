57ad3db3f915a3c3a0ac026b2c942b8e54f26212
Willie Ruemmele <willieruemmele@gmail.com>
2022-10-06 07:48:44 -0600

    chore: encode template, need to get it from api call

--- src/commands/doctor.ts
+++ src/commands/doctor.ts
@@ -143,7 +143,47 @@ export default class Doctor extends SfdxCommand {
 
       const title = await this.ux.prompt('Please enter a title for the issue');
       const url = encodeURI(
-        `https://github.com/forcedotcom/cli/issues/new?title=${title}&template=bug_report.md&labels=doctor,investigating,${this.config.bin}`
+        `https://github.com/forcedotcom/cli/issues/new?title=${title}&body=
+<!--
+NOTICE: GitHub is not a mechanism for receiving support under any agreement or SLA. If you require immediate assistance, please use official support channels.
+-->
+
+### Summary
+
+_Short summary of what is going on or to provide context_.
+
+### Steps To Reproduce:
+
+**Repository to reproduce:** [dreamhouse-lwc](https://github.com/dreamhouseapp/dreamhouse-lwc)
+
+***NOTE:** If your issue is not reproducable by dreamhouse-lwc, i.e. requires specific metadata or files, we **require** a link to a simple Salesforce project repository with a script to setup a scratch org that reproduces your problem.*
+
+1.  This is step 1.
+1.  This is step 2. All steps should start with '1.'
+
+### Expected result
+
+_Describe what should have happened_.
+
+### Actual result
+
+_Describe what actually happened_.
+
+### System Information
+
+${diagnosis.cliConfig.userAgent}
+${diagnosis.versionDetail.pluginVersions.join(os.EOL)}
+SFDX ENV. VARS.
+${diagnosis.sfdxEnvVars.join(os.EOL)}
+SF ENV. VARS.
+${diagnosis.sfEnvVars.join(os.EOL)}
+Windows: ${diagnosis.cliConfig.windows}
+Shell: ${diagnosis.cliConfig.shell}
+Channel: ${diagnosis.cliConfig.channel}
+${diagnosis.cliConfig.userAgent}
+### Additional information
+
+_Feel free to attach a screenshot_.&labels=doctor,investigating,${this.config.bin}`
       );
       this.ux.log("now please copy the above terminal output and place in the 'System Information' section in github");
       await this.ux.prompt("press 'Enter' to continue", { required: false });
